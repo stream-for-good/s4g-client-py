876d58de02e9c86765a2e881ca9baeb10bdc66ec
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-05-16 22:26:59 +0200

    allow having several response in credential API

--- s4gpy/api/credentials.py
+++ s4gpy/api/credentials.py
@@ -6,16 +6,26 @@ class CredentialAPI():
         self.session = session
 
     def get_credentials(self, provider_name):
-        """Get a tuple containing credentials for the supplied provider."""
-        credentials = self.session.get(f'/providers/{provider_name}').json()
-        single_credentials_link = credentials['links'][0]['href']
+        """Get a tuple of containing credentials for the supplied provider, chosen at random"""
+        res = self.get_credentials_all(provider_name)
+        return None if len(res) == 0 else res[0]
 
-        single_credentials = self.session.get(
-            single_credentials_link,
-        ).json()
-        login = single_credentials['credentials']['login']
-        password = single_credentials['credentials']['password']
-        return login, password
+    def get_credentials_all(self, provider_name):
+        """Get a list of tuples containing credentials for the supplied provider."""
+        credentials = self.session.get(f'/providers/{provider_name}').json()
+        res = []
+        for cred in credentials['links']:
+            single_credentials_link = cred['href']
+            try:
+                single_credentials = self.session.get(
+                    single_credentials_link,
+                ).json()
+                login = single_credentials['credentials']['login']
+                password = single_credentials['credentials']['password']
+                res.append((login, password))
+            except JSONDecodeError:
+                continue
+        return res
 
     def get_providers(self):
         """get a tuple containing the names providers for which we have at least a credential available"""
